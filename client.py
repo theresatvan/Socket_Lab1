e67a11b0b93ed27f04bafbf0d816e491b3a5317d
unknown <theresatvan@gmail.com>
2022-10-06 04:33:17 -0400

    Add client.py

---
+++ client.py
@@ -0,0 +1,44 @@
+# import socket module
+from http import client
+from socket import *
+import sys  # In order to terminate the program and receive arguments
+import time
+
+def webClient(args=sys.argv[0:]):
+    try:
+        name, port, filename = args[1], int(args[2]), args[3]
+        clientSocket = socket(AF_INET,SOCK_STREAM)  # Create TCP socket
+        clientSocket.connect((name, port))
+        
+        data = 'GET /' + filename
+        clientSocket.send(data.encode())    # send filename to server socket
+        
+        print('From Server:')
+        recvAll(clientSocket)
+        
+        clientSocket.close()
+        
+    except IndexError:
+        print('Input error.')
+        
+    except ConnectionRefusedError:
+        print('Connection refused.')
+        
+    sys.exit()  # Terminate program
+
+# ensures client receives and prints wholre reponse from server
+def recvAll(clientSocket,timeout=1):
+    begin = time.time()
+    while True:
+        if time.time() - begin > timeout:
+            break
+        
+        response = clientSocket.recv(1024)
+        if response:
+            print(response.decode())
+            begin = time.time()
+        else:
+            time.sleep(1)
+
+if __name__ == '__main__':
+    webClient(args=sys.argv[0:])
--- server.py
+++ server.py
@@ -1,47 +1,42 @@
 # import socket module
 from socket import *
 import sys  # In order to terminate the program
+
 def webServer(port=6789):
     serverSocket = socket(AF_INET, SOCK_STREAM)  # Prepare a server socket
     
-    #Fill in start
-    serverSocket.bind(('', port))
-    serverSocket.listen(1)
-    #Fill in end
+    serverSocket.bind(('', port))  # Bind socket to specified port
+    serverSocket.listen(1)  # Server listens for at most 1 TCP connection
     
     while True:
         # Establish the connection
         print('Ready to serve...')
-        connectionSocket, addr = serverSocket.accept() # Fill in start       #Fill in end
+        connectionSocket, addr = serverSocket.accept()  # Server accepts a connection and returns a new socket object
         try:
-            message = connectionSocket.recv(1024).decode() # Fill in start      #Fill in end
+            message = connectionSocket.recv(1024).decode()  # Read bytes from socket
             filename = message.split()[1]
-            f = open(filename[1:])
-            outputdata = f.read() # Fill in start       #Fill in end
-            # Send one HTTP header line into socket
             
-            #Fill in start
+            print('Client request \n' + message)
+            
+            f = open(filename[1:])
+            outputdata = f.read()  # Retrieve specified file requested from client
+            
+            # Send one HTTP header line into socket
             connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
-            #Fill in end
             
             # Send the content of the requested file to the client
             for i in range(0, len(outputdata)):
                 connectionSocket.send(outputdata[i].encode())
-            
+                
             connectionSocket.close()
+            
         except IOError:
             # Send response message for file not found
-            
-            #Fill in start
             connectionSocket.send('HTTP/1.1 404 Not found\r\n\r\n'.encode())
             connectionSocket.send('File not found'.encode())
-            #Fill in end
             
             # Close client socket
-            
-            #Fill in start
             connectionSocket.close()
-            #Fill in end
     
     serverSocket.close()
     sys.exit()  # Terminate the program after sending the corresponding data
